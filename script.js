f9656c148a28c9c71d8a25edff72b3b9b553d120
Pema Sherpa <pysherpa97@gmail.com>
2022-06-01 21:07:01 -0400

    initial cimmit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var countryName = "Nepal";
+
+// console.log("JS starts execution!");
+// alert("I am waiting for your click!");
+
+// console.log("JS ends execution!");
+
+// console.log(countryName);
+// var countryName = "Nepal";
